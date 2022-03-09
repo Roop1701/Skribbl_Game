1c80b48399931b65607394f998ab0fe76ee9df6d
chatterjee.roopkumar17 <chatterjee.roopkumar17@gmail.com>
2022-03-09 22:14:35 +0530

    Frontend Steup Ready

---
+++ Frontend/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+export default function App() {
+  return (
+    <>
+      <div>Welcome to Skribbl</div>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ Frontend/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./app.js",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.s[ac]ss/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
